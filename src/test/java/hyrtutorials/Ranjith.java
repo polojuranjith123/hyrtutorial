2f7f33c90527ac8755bb4099779045dc908184fe
polojuranjith123 <polojuranjith515@gmail.com>
2021-09-29 14:58:45 +0530

    3rd commit

---
+++ src/test/java/hyrtutorials/Ranjith.java
@@ -0,0 +1,12 @@
+package hyrtutorials;
+
+public class Ranjith {
+
+	public static void main(String[] args) {
+		System.out.println("ranjith");
+		
+		System.out.println("mani");
+		System.out.println("hyma");
+	}
+
+}
